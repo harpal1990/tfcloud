0a4c916a1bf52694e854a3b2ba568d7185af6558
harpal1990 <harpalgohilon@gmail.com>
2023-08-09 08:56:24 +0530

    upload files

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "my-ec2-vm" {
   ami                    = "ami-053b0d53c279acc90"
   instance_type          = var.instance_type
-  count                  = 2
+  count                  = 1
   key_name               = "aws"
   user_data              = file("${path.module}/install_nginx.sh")
   vpc_security_group_ids = ["${aws_security_group.TechVPC_SG.name}"]
